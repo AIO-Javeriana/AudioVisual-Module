4860d3e5d586e3038bf2d4c5d8b356465352f296
Fabian Merchan <merchanf@javeriana.edu.co>
2016-09-22 12:59:58 -0500

    lerp test initiated

--- js/index.js
+++ js/index.js
@@ -1,58 +1,85 @@
-$(function() {
+$(function () {
+  var type = 'svg';
+  var two = new Two({
+    type: Two.Types[type],
+    fullscreen: true,
+    autostart: true
+  }).appendTo(document.body);
 
-          var type = 'svg';
-          var two = new Two({
-            type: Two.Types[type],
-            fullscreen: true,
-            autostart: true
-          }).appendTo(document.body);
+  var dimensions = 500;
+  var scale = two.width > two.height ? two.height / dimensions : two.width / dimensions;
+  var shapes = [];
+  var index = 0;
+  var length = 0;
+  var easing = 0.125;
 
-          var dimensions = 500;
-          var shapes = [];
-          var index = 0;
-          var length = 0;
-          var scale = two.width > two.height ? two.height / dimensions : two.width / dimensions;
-          var easing = 0.125;
+  constructor();
 
-          $('#assets svg').each(function(i, el) {
+  length = shapes.length;
 
-            var shape = two.interpret(el).center();
-            shape.fill = 'black';
-            shape.visible = false;
-            shape.noStroke();
-            shape.translation.set(two.width / 2, two.height / 2);
-            shapes.push(shape);
-            _.each(shape.children, function(child) {
-              _.each(child.vertices, function(v) {
-                v.ox = v.x;
-                v.oy = v.y;
-              });
-            });
+  var shape1 = getShapeById('opened-eye');
+  var shape2 = getShapeById('closed-sleeping-eye');
+  var mostVerticesLength = getMostVertices(shape1, shape2, 0, 0);
+  var leastVerticesLength = getLeastVertices(shape1, shape2, 0, 0);
 
-          });
+  shape1.visible = true;
+  var x_1 = shape1.children[0].vertices[0].x;
+  var y_1 = shape1.children[0].vertices[0].y;
+  vector1 = new Two.Vector(x_1, y_1);
 
-          length = shapes.length;
+  var x_2 = shape2.children[0].vertices[0].x;
+  var y_2 = shape2.children[0].vertices[0].y;
+  vector2 = new Two.Vector(x_2, y_2);
+  
+  console.log(vector1);
+  console.log(vector2);
+  
+  two.bind('update', function () {
 
-          two.bind('update', function() {
+    
+  });
 
-            var shape = shapes[index];
-            shape.visible = true;
-            shape.scale += (scale - shape.scale) * easing;
-
-            _.each(shape.children, function(child) {
-              _.each(child.vertices, function(v) {
-                var x = v.ox + Math.random();
-                var y = v.oy + Math.random();
-                v.set(x, y);
-              });
-            });
-
-            if (shape.scale >= (scale - 0.001)) {
-              shape.visible = false;
-              shape.scale = 0;
-              index = (index + 1) % length;
-            }
-
-          });
-
+  function constructor() {
+    $('svg').each(function (i, el) {
+      var shape = two.interpret(el).center();
+      shape.fill = 'black';
+      shape.visible = false;
+      shape.noStroke();
+      shape.translation.set(two.width / 2, two.height / 2);
+      shapes.push(shape);
+      _.each(shape.children, function (child) {
+        _.each(child.vertices, function (v) {
+          v.ox = v.x;
+          v.oy = v.y;
         });
+      });
+    });
+  }
+
+  function getShapeById(id){
+    var theChosenOne = null;
+    _.each(shapes, function(shape){
+      if(shape.id == id){
+        theChosenOne = shape;
+      }
+    });
+    return theChosenOne;
+  }
+
+  function getMostVertices(shape1, shape2, child1, child2){
+    var length1 = shape1.children[child1].vertices.length;
+    var length2 = shape2.children[child2].vertices.length;
+    if( length1 > length2 )
+      return length1;
+    return length2;
+  }
+
+  function getLeastVertices(shape1, shape2, child1, child2){
+    var length1 = shape1.children[child1].vertices.length;
+    var length2 = shape2.children[child2].vertices.length;
+    if( length1 < length2 )
+      return length1;
+    return length2;
+  }
+
+});
