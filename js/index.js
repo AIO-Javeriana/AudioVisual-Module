2ef1042451fe89a28180b831b95bab00eb5b4608
Fabian Merchan <merchanf@javeriana.edu.co>
2016-09-22 23:38:07 -0500

    implemented looking right and looking left

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
 var morfeo = new  SVGMorpheus('#face-set', {iconId: 'opened-eyes'});
-morfeo.to('test', {rotation: 'none', duration: 1500});
+morfeo.to('looking-left', {rotation: 'none', duration: 500}, function(){
+	morfeo.to('looking-right', {rotation: 'none', duration: 500});
+});
+	
