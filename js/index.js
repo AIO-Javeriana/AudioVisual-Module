995dced645817f3226d6fa66b47f008e0674f1fc
Fabian Merchan <merchanf@javeriana.edu.co>
2016-09-24 13:43:10 -0500

    audioIntput class created, initiating recordjs tests, gone to lunch.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-var morfeo = new  SVGMorpheus('#face-set', {iconId: 'medium-closed-happy-eyes'});
-morfeo.to('full-closed-happy-eyes', {rotation: 'none', duration: 500});
-	
+/*
+    Main method
+*/
+
+
+
+/*  
+    var morfeo = new  SVGMorpheus('#face-set', {iconId: 'medium-closed-happy-eyes'});
+    morfeo.to('full-closed-happy-eyes', {rotation: 'none', duration: 500});
+*/
