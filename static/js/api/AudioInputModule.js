a15ca50c545fe4db547ace591f74312e4ccff712
Fabian Merchan <merchanf@javeriana.edu.co>
2016-11-13 20:53:36 -0500

    deleted recording options, found as useless with addition of speech to text

--- static/js/api/AudioInputModule.js
+++ static/js/api/AudioInputModule.js
@@ -11,56 +11,7 @@ class AudioInputModule extends Module{
      * @param mediaStreamSource media stream source needed to record.
      */
     constructor(id, mediaStreamSource) {
-        super(id);
-        this.recording = false;
-        /*this.record = new Recorder(mediaStreamSource, {
-            workerPath: "./js/external/recorderWorker.js"
-        });*/
-    
-        /**
-         * Starts to record.
-         */
-        this.startRecording = function(){
-            this.record.clear();
-            this.record.record();
-        }
-
-        /**
-         * Stops the recording.
-         */
-        this.stopRecording = function() {
-            this.record.stop();
-        }
-
-        /**
-         * Prepares the recording file to be downloaded.
-         * @param recording's name
-         */
-        this.exportRecording = function(name) {
-            this.record.exportWAV(function (blob) {
-                var url = URL.createObjectURL(blob);
-                var download = document.createElement('a');
-                download.href = url;
-                download.download = name + '.wav';
-                download.click();
-            });
-        }
-
-        /**
-         * A play, stop and exportRecording implementation, if not recording
-         * starts to record. If recording, stops and saves the recording.
-         * @param name recording's name
-         */
-        this.toggleRecord = function(name){
-            if (!this.recording) {
-                this.startRecording();
-            } else {
-                this.stopRecording();
-                this.exportRecording(name);
-            }
-            this.recording = !this.recording;
-        }
-        
+        super(id);        
         
         this.speechToText = function(callback){
             var recognition = new webkitSpeechRecognition();
