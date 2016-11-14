52ebe66e9a4ad726078825038615ed206f019665
lespoir <stiven140@hotmail.com>
2016-11-14 20:14:35 +0000

    spanish translate added

--- static/js/api/AudioInputModule.js
+++ static/js/api/AudioInputModule.js
@@ -35,15 +35,35 @@ class AudioInputModule extends Module{
         this.answer = function(processFunction, callback){
             this.speechToText(function(question){
                 $.ajax({
-                    url: "https://webknox-question-answering.p.mashape.com/questions/answers",
-                    data: { answerLookup: false, answerSearch: false, question: question},
+                    url: "https://translate.yandex.net/api/v1.5/tr.json/translate",
+                    data: { key: "trnsl.1.1.20161114T200315Z.3c3ab99e5b147085.058271e128f7f2506533bc569277afbf4ff14029", 
+                            text: question, 
+                            lang: "es-en"
+                    },
                     type: "GET",
-                    beforeSend: function(xhr)
-                    {
-                        xhr.setRequestHeader('X-Mashape-Key', 'LjjvxSR6ZHmshpvD3uHwyArX5Supp1I6RZdjsnY8w8LNQC2SUe');
-                    }
                 }).done(function( data ) {
-                    processFunction(data.answer, callback);
+                    console.log(data.text[0]);
+                    $.ajax({
+                        url: "https://webknox-question-answering.p.mashape.com/questions/answers",
+                        data: { answerLookup: false, answerSearch: false, question: data.text[0]},
+                        type: "GET",
+                        beforeSend: function(xhr)
+                        {
+                            xhr.setRequestHeader('X-Mashape-Key', 'LjjvxSR6ZHmshpvD3uHwyArX5Supp1I6RZdjsnY8w8LNQC2SUe');
+                        }
+                    }).done(function( data ) {
+                        $.ajax({
+                            url: "https://translate.yandex.net/api/v1.5/tr.json/translate",
+                            data: { key: "trnsl.1.1.20161114T200315Z.3c3ab99e5b147085.058271e128f7f2506533bc569277afbf4ff14029", 
+                                    text: data.answer, 
+                                    lang: "en-es"
+                            },
+                            type: "GET",
+                        }).done(function( data ) {
+                            console.log(data.text);
+                            processFunction(data.text, callback);
+                        });
+                    });
                 });
             });
         }
